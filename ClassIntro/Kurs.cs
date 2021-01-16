072ea95b01e24dbd7ef4b85f51b2f0806aabbe97
muyessercancelik <muyessercancelik@MUYESSER13>
2021-01-17 00:01:55 +0300

    16 Ocak Ders Sonu

---
+++ ClassIntro/Kurs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassIntro
+{
+    class Kurs
+    {
+        public string KursAdi { get; set; }
+
+        public string Egitmen { get; set; }
+
+        public int IzlenmeOrani { get; set; }
+
+
+
+    }
+}
---
+++ ClassIntro/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ClassIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string adi = "Müyesser";
+            int yas = 28;
+
+            Kurs kurs1 = new Kurs();
+            kurs1.KursAdi = "C#";
+            kurs1.Egitmen = "Engin Demiroğ";
+            kurs1.IzlenmeOrani = 68;
+
+            Kurs kurs2 = new Kurs();
+            kurs2.KursAdi = "Yazılım";
+            kurs2.Egitmen = "Atınç Hoca";
+            kurs2.IzlenmeOrani = 30;
+        }
+    }
+}
