3573f3f344ff3ab6c6037a91d4e6d4a53bf454dc
muyessercancelik <muyessercancelik@MUYESSER13>
2021-01-17 16:28:49 +0300

    Kamp 2. Gün

---
+++ Donguler/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Donguler         //Birbirini tekrar eden alanlar için döngüleri kullanırız.
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kurs1 = "Yazılım Geliştirici Yetiştirme Kampı";
+            string kurs2 = "Programlamaya başlangıç için temel kurs";
+            string kurs3 = "Java";
+            string kurs4 = "Python";
+            string kurs5 = "C++";
+
+            Console.WriteLine(kurs1);
+            Console.WriteLine(kurs2);
+            Console.WriteLine(kurs3);
+            Console.WriteLine(kurs4);
+            Console.WriteLine(kurs5);   // Manuel yazdık asla böyle kullanılmaz. 
+
+            //array - dizi kullanılır.
+
+            string[] kurslar = new string[] { "Yazılım Geliştirici Yetiştirme Kampı", "Programlamaya başlangıç için temel kurs", "Java", "Python", "C++" };
+
+            for (int i = 0; i < kurslar.Length; i++)
+            {
+                Console.WriteLine(kurslar[i]);
+            }
+
+            Console.WriteLine("For Bitti");
+            foreach (string kurs in kurslar)  //Dizi temelli yapıları tek tek dönmeye(dolaşmaya) yarar. Dizilere uygulanır.kurs=alians - yani takma isim, istediğin adı verebilirsin.
+            {
+                Console.WriteLine(kurs);    //Sektörde genellikle dizi dolaşmak için foreach kullanılır. Ama for da kullanan var. Foreach kolay dolaşır.
+            }
+
+            Console.WriteLine("Footer Alanı");
+
+
+                                            //i+=2 olarak da yazabilirsin aynı işlem.
+           // for (int i = 1; i <= 10; i=i+2)  //Javadaki for ile aynı kod bloğu - 1den başlayacak ve i<10 şartını sağlayana kadar devam et ve 1 1 artır.
+           // {
+           //     Console.WriteLine(i);   //i=1 i<10 mu? evet o zaman 1 artır. 10<10 mu? hayır. o zaman döngü durur.
+           // }
+        }
+    }
+}
--- KampIntro/Program.cs
+++ KampIntro/Program.cs
@@ -7,16 +7,39 @@ namespace KampIntro
         static void Main(string[] args)
         {
             //type safety - tip güvenliği
+            //string kategoriEtiketi = "Kategori";
+            //değer tutucu, alias
+            //Kodlar yukarıdan aşağıya doğru çalışır.
+
             string kategoriEtiketi = "Kategoriler";
-            int ogrenciSayisi = 32000;
-            double faizOrani = 1.45;
+            int ogrenciSayisi = 32000; //int tam sayıları tutmak için
+            double faizOrani = 1.45;  //ondalıklı sayıları tutmak için
 
 
-            bool sistemeGirisYapmisMi = false;
+            bool sistemeGirisYapmisMi = true;   //Buradaki true/false gerçek hayatta bir veri kaynağından gelmektedir.
+            double dolarDun = 7.35;
+            double dolarBugun = 7.45;
 
-            if (sistemeGirisYapmisMi == true)
+            if (dolarDun>dolarBugun)
             {
-                Console.WriteLine("Kullanıcı Ayarları butonu");
+                Console.WriteLine("Azalış Butonu");
+            }
+            else if (dolarDun<dolarBugun)
+            {
+                Console.WriteLine("Artış Butonu");
+            }
+            else
+            {
+                Console.WriteLine("Eşittir Butonu");
+            }
+
+
+
+
+            if (sistemeGirisYapmisMi == true)  //Parantez içi şartım. Şartım gerçekleşiyorsa aşağıdaki süslü parantez içini çalıştır demek.
+                                                // İki kere taba bas if şart bloğunu otomatik olarak ekler.
+            {
+                Console.WriteLine("Kullanıcı Ayarları butonu");  //Burada iki şart var, ya if ya da else çalışır. İkisi aynı anda çalışmaz.
             }
             else
             {
