7df61c33b7bb524036424e1340d4c7b08573853d
muyessercancelik <muyessercancelik91@gmail.com>
2021-01-30 15:30:03 +0300

    30.01.2021

--- OOP3/BasvuruManager.cs
+++ OOP3/BasvuruManager.cs
@@ -16,6 +16,14 @@ namespace OOP3
             //Bu şekilde yaparsak çalışır ancak tüm başvurular Konut kredisi üzerinden çalışır.
             //Ancak BasvuruYap kısmına (IKrediManager krediManager) olarak güncellersek
             // ve hesaplamayı şu şekilde çağırırsak;
+            //Hem veri tabanına loglayalım hem müşteriye sms atalım denebilir.O zaman ILoggerService i List<> olarak yazmalısın. Bir tane değil çok yollamak istediğin için.
+            //public void BasvuruYap(IKrediManager krediManager, List<ILoggerService> loggerService)
+            //foreach (var loggerService in loggerServices)
+            //{
+            //         loggerService.Log(); Artık tek değil çoklu loggerla çalışıyoruz.Program.csde de ona göre düzenleme yaparsın.
+            //}
+
+
             krediManager.Hesapla();
             loggerService.Log();
 
